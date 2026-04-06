ce751c0729a4d1eef91c38b83a57d8a3aacc3dbd
Abdaal Ali <136990423+abdaal12@users.noreply.github.com>
2026-04-06 14:16:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ const app = express();
 //  CORS - Allow frontend to talk to backend
 // Only allows requests from our React app
 app.use(cors({
-  origin:      process.env.CLIENT_URL || 'http://localhost:3000',
+  origin:      process.env.CLIENT_URL || [
+    "http://localhost:3000",
+    "https://chickfrontend.vercel.app"
+  ],
   credentials: true,
   methods:     ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
